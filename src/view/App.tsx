af1699ed4c331755cf2d4cc2eb2a7b5a24110085
haroro <hutao-haroro@qq.com>
2019-08-01 09:04:11 +0800

    1，新增react-router@typescript

--- src/view/App.tsx
+++ src/view/App.tsx
@@ -1,15 +1,49 @@
 import * as React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import '@/view/App.scss';
 
 // let logo = require('@/assets/img/logo.svg')
+
+function Index () {
+  return <h2>Home</h2>;
+}
+
+function About () {
+  return <h2>About</h2>;
+}
+
+function Users () {
+  return <h2>Users</h2>;
+}
 
 class App extends React.Component {
   render () {
     return (
       <div className="App">
         <header className="header-box">
-          <span>花呗分期</span>
+          <span>导航栏</span>
         </header>
+        <Router>
+          <div>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/about/">About</Link>
+                </li>
+                <li>
+                  <Link to="/users/">Users</Link>
+                </li>
+              </ul>
+            </nav>
+
+            <Route path="/" exact component={Index} />
+            <Route path="/about/" component={About} />
+            <Route path="/users/" component={Users} />
+          </div>
+        </Router>
       </div>
     )
   }
